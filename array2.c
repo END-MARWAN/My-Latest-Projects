db92d9d1ba27cf95a67c70fb87b872f5ca2811aa
Marwan Abd elazeem <marwanahmed2006.0@gmail.com>
2024-09-25 03:07:58 +0300

    Add files via upload

---
+++ array2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+     int *list = malloc( 3*sizeof(int)) ;
+     if ( list == NULL){
+          return 1;
+     }
+      list[0] = 1;
+      list[1] = 2;
+      list[2] = 3;
+     int *tmp= realloc (list,4*sizeof(int));
+    if ( tmp == NULL){
+          free (list);
+          return 1;
+  }
+    tmp [3] = 4;
+   list = tmp ;
+   for ( int i = 0 ; i < 4; i++ ){
+     printf("%i\n", list [i]);
+   }
+}
+
