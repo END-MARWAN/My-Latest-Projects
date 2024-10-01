25289e5a45843e4f5720a3c68ac234f025daa1fc
Marwan Abd elazeem <marwanahmed2006.0@gmail.com>
2024-10-01 17:54:29 +0300

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,4 @@
+from cs50 import get_string
+
+answer = get_string ("what's your name? ")
+print(f"hello, {answer} how are you?")
---
+++ hello2.py
@@ -0,0 +1,9 @@
+x = 30
+y = 20
+if x<y:
+     print("x is less than y")
+elif x == y:
+     print("x = y")
+
+else:
+     print("x is not less than y")
