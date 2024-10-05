327a8917180a6d9670abb59efd79330e8cd0df5e
Marwan Abd elazeem <marwanahmed2006.0@gmail.com>
2024-10-06 01:11:01 +0300

    Add files via upload

---
+++ hello4.py
@@ -0,0 +1,6 @@
+# type     brackets     ordered     mutable     duplicate            use
+
+#list         []          yes         yes          yes               عام
+#tuple        ()          yes         no           yes             احداثيات
+#set          {}          no          no           no          العمليات الرياضية
+#dict         {}          key         yes          no/yes       قواعد البيانات
